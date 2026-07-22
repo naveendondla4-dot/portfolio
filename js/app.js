9cc310c85da583cbb127f4d08ecdb3740ee8cc83
naveendondla4-dot <naveendondla4@gmail.com>
2026-07-22 20:39:43 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -122,13 +122,15 @@ document.getElementById("contactForm").addEventListener("submit", async function
 
     try {
 
-        const response = await fetch("http://127.0.0.1:8000/contact", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        });
+        const response = await fetch("https://portfolio-api-production-ab15.up.railway.app/contact", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+});
+
+         
 
         const result = await response.json();
 
